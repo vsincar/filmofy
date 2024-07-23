402c506dfc4e45d1ef7efc419885931cdee6fa0a
vsincar <vsincar@gmail.com>
2024-07-23 16:06:56 +0300

    placeholder animation added

---
+++ scripts/placeholder_anim.js
@@ -0,0 +1,42 @@
+let typedInstance;
+
+// Placeholder animasyonunu başlat
+function startAnimation() {
+    if (!typedInstance) {
+        typedInstance = new Typed('#animated-placeholder', {
+            strings: ['Arama yapın...', 'Film adı girin...', 'Filmleri filtreleyin...'],
+            typeSpeed: 50,
+            backSpeed: 50,
+            backDelay: 1000,
+            startDelay: 500,
+            loop: true
+        });
+    }
+}
+
+// Animasyonu durdur
+function stopAnimation() {
+    if (typedInstance) {
+        typedInstance.destroy();
+        typedInstance = null; // Reset the typedInstance
+    }
+    // Placeholder metnini temizle
+    document.getElementById('animated-placeholder').placeholder = '';
+}
+
+// Sayfa yüklendiğinde animasyonu başlat
+document.addEventListener('DOMContentLoaded', startAnimation);
+
+// Input alanına tıklanıldığında animasyonu durdur ve placeholder metnini temizle
+document.getElementById('animated-placeholder').addEventListener('focus', function () {
+    stopAnimation();
+});
+
+// Sayfa üzerinde herhangi bir yere tıklanıldığında animasyonu yeniden başlat (eğer input boşsa)
+document.addEventListener('click', function (event) {
+    const inputElement = document.getElementById('animated-placeholder');
+    // Eğer tıklanan yer input dışıysa ve input alanı boşsa animasyonu tekrar başlat
+    if (!inputElement.contains(event.target) && inputElement.value === '') {
+        setTimeout(startAnimation, 100);
+    }
+});
