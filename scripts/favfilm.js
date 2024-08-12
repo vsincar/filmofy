ee4f11276c9107ab8d5f9053d78ec8b6780cdfa1
vsincar <vsincar@gmail.com>
2024-08-12 12:13:43 +0300

    yenilendi

--- scripts/favfilm.js
+++ scripts/favfilm.js
@@ -1,23 +1,28 @@
 // favfilm.js
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('database/films.json')
-        .then(response => response.json())
-        .then(data => {
-            const carouselInner = document.querySelector('#carouselFavorites .carousel-inner');
-            let carouselItems = '';
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("database/films.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const carouselInner = document.querySelector(
+        "#carouselFavorites .carousel-inner"
+      );
+      let carouselItems = "";
 
-            // Generate carousel items
-            data.forEach((film, index) => {
-                // Check if the index is divisible by 5 to start a new carousel item
-                if (index % 5 === 0) {
-                    if (index > 0) {
-                        carouselItems += '</div>'; // Close previous carousel item
-                    }
-                    carouselItems += '<div class="carousel-item' + (index === 0 ? ' active' : '') + '"><div class="row">';
-                }
+      // Generate carousel items
+      data.forEach((film, index) => {
+        // Check if the index is divisible by 5 to start a new carousel item
+        if (index % 5 === 0) {
+          if (index > 0) {
+            carouselItems += "</div>"; // Close previous carousel item
+          }
+          carouselItems +=
+            '<div class="carousel-item' +
+            (index === 0 ? " active" : "") +
+            '"><div class="row" style="background-color:white">';
+        }
 
-                // Add film card
-                carouselItems += `
+        // Add film card
+        carouselItems += `
                     <div class="col">
                         <div class="card">
                             <img src="${film.image}" class="card-img-top" alt="${film.name}">
@@ -25,14 +30,14 @@ document.addEventListener('DOMContentLoaded', function () {
                     </div>
                 `;
 
-                // Close the last carousel item
-                if (index === data.length - 1 || (index + 1) % 5 === 0) {
-                    carouselItems += '</div></div>';
-                }
-            });
+        // Close the last carousel item
+        if (index === data.length - 1 || (index + 1) % 5 === 0) {
+          carouselItems += "</div></div>";
+        }
+      });
 
-            // Inject carousel items into carousel inner
-            carouselInner.innerHTML = carouselItems;
-        })
-        .catch(error => console.error('Error fetching the films data:', error));
+      // Inject carousel items into carousel inner
+      carouselInner.innerHTML = carouselItems;
+    })
+    .catch((error) => console.error("Error fetching the films data:", error));
 });
